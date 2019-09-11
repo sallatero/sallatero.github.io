508324e5990f201ae10fac54d75c2c39750d0cdf
Salla <salla.tero@gmail.com>
2019-09-11 15:06:45 +0300

    Story page's button made to work also as read less

--- src/Story.js
+++ src/Story.js
@@ -54,6 +54,33 @@ const Story = (props) => {
         that have made me who I am today.
       </p>  
       <div className='button slide-in-top ab-but' onClick={() => toggleVisibility()}>Read more</div>
+      <p className='slide-out-top'>
+        I have a technical background and started my career in IT consultancy. I gained experience in 
+        working with different kinds of client projects in a multicultural environment. I designed new 
+        services and processes, coordinated system deployments, configured software products and conducted 
+        user testing. After some years I wanted to try something else, since I felt that there was something missing.
+      </p>
+      <p className='slide-out-top'>
+        As I was very interested in health and personal development at the time, I decided to study 
+        and become a health coach. Running my own business was exciting and I enjoyed the freedom and 
+        responsibility. I had found something valuable. However, building a business from scratch was 
+        difficult. I knew I wanted to be in charge of my own work, but I needed more support and structure.
+        So when I had the possibility to try real estate, I jumped at the chance. 
+      </p>
+      <p className='slide-out-top'>
+        Life as a real estate agent was fun! I met a lot of people and enjoyed helping them to buy or sell 
+        their home. I loved building my expertise in the applicable laws, home styling, marketing, and sales. 
+        I got more insight into what makes me unique, what my strengths are and how I can utilise them best. 
+        It turned out real estate was mostly about sales and landing new clients. There was not enough time 
+        to focus on taking care of the existing client assignments, which was my place to shine.
+      </p>
+      <p className='slide-out-top'>
+        Different kinds of experiences and my years of self-examination have given me clarity of who I am, 
+        what drives me, what inspires me, and what my strengths are. I have come to understand that
+        my future is in IT. Since I am excited about new apps and services and love coding, I am now looking 
+        for a place to grow my expertise in web development, especially frontend. I have great motivation to
+        learn and challenge myself and am ready to work hard for my dream.
+      </p>    
     </div>
     }
     </div>
