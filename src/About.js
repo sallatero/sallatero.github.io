7379fc451f331502b6543b39560c04221a48e2d5
Salla <salla.tero@gmail.com>
2019-09-11 12:08:06 +0300

    About page's slide in top: li's come at the same time, p's also use slide in top

--- src/About.js
+++ src/About.js
@@ -7,29 +7,29 @@ const About = (props) => {
       <div className='lead-text'>
         <div className='lead-text-heading tracking-in-expand'>Multitalented IT engineer with </div>
         <ul>
-          <li className='slide-in-top line1'><span><i class="fas fa-map-marker"></i></span>a versatile experience in project work and customer service</li>
-          <li className='slide-in-top line2'><span><i class="fas fa-map-marker"></i></span>a passion for self developement. I continuously strive to utilise my potential better. </li> 
-          <li className='slide-in-top line3'><span><i class="fas fa-map-marker"></i></span>a dream to make people happier by making their every day life easier. This I can do with 
+          <li className='slide-in-top ab'><span><i class="fas fa-map-marker"></i></span>a versatile experience in project work and customer service</li>
+          <li className='slide-in-top ab'><span><i class="fas fa-map-marker"></i></span>a passion for self developement. I continuously strive to utilise my potential better. </li> 
+          <li className='slide-in-top ab'><span><i class="fas fa-map-marker"></i></span>a dream to make people happier by making their every day life easier. This I can do with 
             designing and implementing new digital services and applications.</li></ul>
       </div>
-      <p className='fade-in p1'>I have a technical background and started my career in IT consultancy. I gained experience in 
+      <p className='slide-in-top p'>I have a technical background and started my career in IT consultancy. I gained experience in 
         working with different kinds of client projects in a multicultural environment. I designed new 
         services and processes, coordinated system deployments, configured software products and conducted 
         user testing. After some years I wanted to try something else, since I felt that there was something missing.</p>
       
-      <p className='fade-in p2'>As I was very interested in health and personal development at the time, I decided to study 
+      <p className='slide-in-top p'>As I was very interested in health and personal development at the time, I decided to study 
       and become a health coach. Running my own business was exciting and I enjoyed the freedom and 
       responsibility. I had found something valuable. However, building a business from scratch was 
       difficult. I knew I wanted to be in charge of my own work, but I needed more support and structure.
       So when I had the possibility to try real estate, I jumped at the chance. </p>
 
-      <p className='fade-in p3'>Life as a real estate agent was fun! I met a lot of people and enjoyed helping them to buy or sell 
+      <p className='slide-in-top p'>Life as a real estate agent was fun! I met a lot of people and enjoyed helping them to buy or sell 
         their home. I loved building my expertise in the applicable laws, home styling, marketing, and sales. 
         I got more insight into what makes me unique, what my strengths are and how I can utilise them best. 
         It turned out real estate was mostly about sales and landing new clients. There was not enough time 
         to focus on taking care of the existing client assignments, which was my place to shine.</p>
 
-        <p className='fade-in p4'>Different kinds of experiences and my years of self-examination have given me clarity of who I am, 
+        <p className='slide-in-top p'>Different kinds of experiences and my years of self-examination have given me clarity of who I am, 
           what drives me, what inspires me, and what my strengths are. I have come to understand that
           my future is in IT. Since I am excited about new apps and services and love coding, I am now looking 
           for a place to grow my expertise in web development, especially frontend. I have great motivation to
