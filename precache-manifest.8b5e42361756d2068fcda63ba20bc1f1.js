9f3d721c68c5d4b964b114763cd946b3d88ca0e6
Salla <salla.tero@gmail.com>
2019-09-13 08:24:08 +0300

    Updates

---
+++ precache-manifest.8b5e42361756d2068fcda63ba20bc1f1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eedafebb623bc3afbd27ac59dc7bd52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e5556ebe8dde13eb627",
+    "url": "/static/css/main.6cc7ed8d.chunk.css"
+  },
+  {
+    "revision": "2677cc863efafda5a889",
+    "url": "/static/js/2.4723d537.chunk.js"
+  },
+  {
+    "revision": "2e5556ebe8dde13eb627",
+    "url": "/static/js/main.a038f72c.chunk.js"
+  },
+  {
+    "revision": "9fddbb50441e6791a55c",
+    "url": "/static/js/runtime~main.e159818f.js"
+  },
+  {
+    "revision": "0509c0c12998ab081d5ef286b4316814",
+    "url": "/static/media/01-small-b.0509c0c1.jpg"
+  },
+  {
+    "revision": "938da0d4f1d05e10c8622ef41ad601c4",
+    "url": "/static/media/02-small-b.938da0d4.jpg"
+  },
+  {
+    "revision": "31dfe188d9167c741eed37d0425663fb",
+    "url": "/static/media/03-small-b.31dfe188.jpg"
+  },
+  {
+    "revision": "8fdc6588461943c094ec92fb8be5f7e7",
+    "url": "/static/media/04-small.8fdc6588.jpg"
+  },
+  {
+    "revision": "38a87f79010d743410d88ef80fe986c6",
+    "url": "/static/media/BG_img_3a.38a87f79.jpg"
+  }
+]);
